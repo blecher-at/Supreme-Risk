8aff7575c3d08b221f41068161d8003055fed3c5
U-EPSILON10\blecher <stephan@blecher.at>
2019-11-13 20:37:31 +0100

    win conditions fixed

--- Supreme_Risk.v0010/Supreme_Risk_script.lua
+++ Supreme_Risk.v0010/Supreme_Risk_script.lua
@@ -36,11 +36,11 @@ local executing = false
 local baseSizeMeters = 400;
 local roundIdleSeconds = 0; -- this round is idle for n seconds now
 local maxRoundIdleTime = 45; -- number of seconds from last round action to begin next round
-local idleWarnMin = maxRoundIdleTime / 2; -- warn n seconds before end of round
+local idleWarnMin = maxRoundIdleTime / 4; -- warn n seconds before end of round
 local idleWarnMax = maxRoundIdleTime - 2; -- warn n seconds before end of round
 local roundnum = 1;
 local roundTotalTime = 0;
-
+local SRGameRunning = true; -- set to false on win
 local players = {};
 
     function basicSerialize (o)
@@ -265,7 +265,7 @@ function OnPopulate()
 	-- spawn Trees
 --	spawnTrees()
 		LOG("OPTIONS")
-	dump(ScenarioInfo.Options)
+	--dump(ScenarioInfo.Options)
 
 	
 	-- prevent ACU warpin animation
@@ -298,7 +298,6 @@ function OnPopulate()
 	for a, ccc in ScenarioUtils.AreaToRect('AREA_1') do LOG(a.." "..ccc) end
 
 	LOG("OPTIONS")
-	dump(ScenarioInfo.Options)
 	LOG("ONPOPULATE END")
 end
 
@@ -433,7 +432,7 @@ end
 
 function setPlayerRestrictions( index, SRUnitsToBuild) 
 		
-		dump(categories)
+		--dump(categories)
 		--Building Restrictions
 		ScenarioFramework.AddRestriction(index, categories.ALLUNITS)
 		ScenarioFramework.RemoveRestriction(index, categories[units.primary]) -- mech marine
@@ -1314,6 +1313,7 @@ function checkCountryOwnership()
 							-- HOME BASES (Have units stay there for a while, and only move one territory per round), init at the beginning of the round
 							if not unit.homebase or roundTotalTime == 0 or (cdata.conqueredThisRound and ScenarioInfo.Options.SRUnitMovement == "agro") then
 								unit.homebase = cdata
+								LOG("assigning -- "..ScenarioInfo.Options.SRUnitMovement.." --from "..cdata.name)
 							end
 							
 							if cdata.president != unit then
@@ -1326,7 +1326,7 @@ function checkCountryOwnership()
 									unit:SetImmobile(false)
 								else
 	--								unit:SetImmobile(true) -- unit:SetSpeedMult(0.4) -- have it stay here for a while		
-									unit:SetSpeedMult(0.2) -- dont move a lot anymore
+									unit:SetSpeedMult(0.1) -- dont move a lot anymore
 									LOG("RESTING -- "..ScenarioInfo.Options.SRUnitMovement)
 									unit:SetCustomName("Unit retreating from "..unit.homebase.name.." paused. Will move again next round.") --Citizen of "..cdata.name)
 								end
@@ -1481,17 +1481,15 @@ function checkEndOfRound()
 		beginNextRound()
 	end
 	
-	displayRoundCountdown(0)
+	if math.floor(roundIdleSeconds/2)*2 == roundIdleSeconds then
+		displayRoundCountdown(1)
+	end
 	
 	if roundIdleSeconds == 10 then
 		local ileft = maxRoundIdleTime - roundIdleSeconds
 	
 		PrintText("Round "..roundnum.." - Liberate Territories to get more reinforcements.", 20, 'FFFFFFFF',ileft - 2,'centertop')
 	end
-
-	-- display after 5 seconds into each round
-	--if roundIdleSeconds == 5  then displayRoundCountdown(5) end
-	--if roundIdleSeconds == 15 then displayRoundCountdown(2) end
 end
 
 function displayRoundCountdown(staytime)
@@ -1503,7 +1501,13 @@ function displayRoundCountdown(staytime)
 	-- prefix seconds
 	if (ileft < 10) then ileft = "0"..ileft end
 	
-	PrintText("Reinforcements arrive in 0:"..ileft.."", 20, '00DDDDDD', staytime, 'centertop') 
+	local player = players[GetFocusArmy()]
+
+	if player.acu.SRUnitsToBuild > 0 and ScenarioInfo.Options.SRBuildMode == 'expire' then
+		PrintText(player.acu.SRUnitsToBuild.." unbuilt units, will be lost in 0:"..ileft.."", 20, '00FFDDDD', staytime, 'centertop') 
+	else
+		PrintText("Reinforcements arrive in 0:"..ileft.."", 20, '00DDDDDD', staytime, 'centertop') 
+	end
 end
 
 function displayMissions()
@@ -1512,7 +1516,7 @@ function displayMissions()
 	local missionText = mission:getText()
 	
 	PrintText('                                           ', 60,'FF0000FF', 999999,'centertop') -- to move text down
-	PrintText(missionText, 30, '00FF4040',999999,'centertop') 
+	PrintText('Mission: '..missionText, 30, '00FF5555',999999,'centertop') 
 	
 end
 
@@ -1524,13 +1528,12 @@ function displayRoundBegin()
 	local missionText = player.mission:getText()
 	
 	local objTitle = "Round "..roundnum..' - Reinforce your territories - you can build '..player.acu.SRUnitsToBuild..' units. '
-	if player.build then
-		if player.build.starting then
-			objTitle = objTitle..'(Initial Units)'
-		else
-			objTitle = objTitle..'('..player.build.total..' this round, '..player.build.ter..' from territories, '..player.build.cont..' from continents, '..player.build.bonus..' from wreckage)'
-		end
+	if not player.build or roundnum == 1 then
+		objTitle = objTitle..'(Initial Units)'
+	else
+		objTitle = objTitle..'('..player.build.total..' this round, '..player.build.ter..' from territories, '..player.build.cont..' from continents, '..player.build.bonus..' from wreckage)'
 	end
+	
 	PrintText(objTitle, 20, 'FFFFFFFF', 9, 'centertop')
 		
 end
@@ -1630,38 +1633,35 @@ function beginNextRound()
 end
 
 function checkPlayerDeath(player)
--- this is buggy
-	if player.empireSize == 0 then
+	if player.empireSize == 0 or player.killedByMission then
 		if not player.acu:IsDead() then 
 			player.acu:Kill() -- goodbye ACU
 		end
---		player.brain:OnDefeat()
+		player.brain:OnDefeat()
 	end
 end
 
 function checkPlayerWin(player)
 	LOG("checking win on "..player.mission:getText())
-	if player.mission:check() and not player.acu:IsDead() then
-		PrintText(player.brain.Nickname.." won: "..player.mission:getText(),20,'FFFFFFFF',5,'center') 
+	if SRGameRunning and player.mission:check() and not player.acu:IsDead() then
+		PrintText(player.brain.Nickname.." won: "..player.mission:getText(),20,'FFFFFFFF',500,'center') 
 
 		-- Kill all other units!
 		player.acu:SetIntelRadius('Vision', 2000)
-		continents = {} -- destroy countries to prevent respawns
+		--continents = {} -- destroy countries to prevent respawns
 		WaitSeconds(1)		
 		
-		for i, player in players do
-			player.brain.OnDefeat = player.brain.OnOrigDefeat -- restore defeat
-		end
-
-		local otherUnits = GetUnitsInRect(Rect(0,0,1024,1024))
-		for i, unit in otherUnits do
-			if not IsAlly(player.index, unit:GetArmy()) then
-				unit:Kill()
-				--player.brain:OnDefeat()
+		for i, otherplayer in players do
+			otherplayer.brain.OnDefeat = otherplayer.brain.OnOrigDefeat -- restore defeat
+			if otherplayer != player then
+				player.killedByMission = true
 			end
-		end		
-
-		WaitSeconds(500)
+		end
+		
+		SRGameRunning = false
+		player.brain:OnVictory()
+		
+		--WaitSeconds(500)
 	end
 end
 
@@ -1713,9 +1713,15 @@ function updateSecondaryMissions()
 			-- only do in own sim state, not for others (this doesnt desync!)
 			
 			-- warn player if he has still not built his units
-			if not player.acu:IsDead() and player.acu.SRUnitsToBuild > 0 and (roundIdleSeconds == 10 or roundIdleSeconds == 20) then
+			if ScenarioInfo.Options.SRBuildMode == 'expire' 
+				and not player.acu:IsDead() 
+				and player.acu.SRUnitsToBuild > 0 
+				and (roundIdleSeconds == 10 or roundIdleSeconds == 20) then
+
 				player.buildObjectiveWarn = true
-				local m1 = {{text = '<LOC E01_M01_060_010>You have '..player.acu.SRUnitsToBuild..' units left to build, Sir.', 
+				--PrintText(player.acu.SRUnitsToBuild.." unbuilt units, will be lost in 0:"..ileft.."", 20, 'FFFFDDDD', staytime, 'centertop') 
+				local ileft = maxRoundIdleTime - roundIdleSeconds
+				local m1 = {{text = '<LOC E01_M01_060_010>You have '..player.acu.SRUnitsToBuild.." unbuilt units, will be lost in 0:"..ileft.."", 
 				vid = 'E01_EarthCom_M01_01131.sfd', bank = 'E01_VO', 
 				cue = 'E01_EarthCom_M01_01131', faction = 'UEF'}}
 				
@@ -1762,20 +1768,20 @@ end
 
 function checkEndOfGame()
 	for i, player in players do
-		checkPlayerDeath(player)
 		checkPlayerWin(player)
+		checkPlayerDeath(player)
 	end
 end
 
 function mainThread()
-
+	SRGameRunning = true
 	-- We are in the game!
 	displayMissions()
 	displayRoundBegin()
 
 	zoomOut()
 
-	while true do
+	while SRGameRunning do
 		checkCountryOwnership()
 		
 		checkEndOfRound()
@@ -1788,7 +1794,7 @@ function mainThread()
 end
 
 function jobsThread()
-	while true do
+	while SRGameRunning do
 		reassignFactories()
 		checkTeleportationZones()
 		controlAIPlayers()
